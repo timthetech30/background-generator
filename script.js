364e0561101b6c88dd7bdf896698298dc6193800
timthetech30 <timthetech30@gmail.com>
2024-01-29 18:08:28 -0600

    adding starting project

---
+++ script.js
@@ -0,0 +1,35 @@
+//command line
+//dir for everything in current direcoty
+//cd for change directory
+//node script.js for running script
+//cls to clear screen 
+//cd / 
+//cd ~ user directory
+//cd .. 
+//in windows use \ and not / for windows path 
+
+//mkdir  makes a folder
+//rmdir removes a folder
+//touch makes a file
+//rm removes a file
+//echo writes to the terminal
+//clear clears the terminal
+//cls to clear screen
+//echo > index.html creates html file in windows
+//start . opens the file we're currently in (open .  on mac)
+//start index.html opens the page
+//open an app like sumblime text
+//start -a "Sublime Text"
+//-a for line 'a' , then "application name"
+//mv a file (mv for mac )
+//rename index.html about.html in windows
+
+
+//what is a pull request
+//what is a commit
+//what is a branch
+//what is a merge
+//what is a pull request
+
+
+
